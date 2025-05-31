172806646c2c8a7d06b959e4e0b9deaeb247cbb8
Vamshi Rapolu <rvamshi1711@gmail.com>
2025-05-31 13:23:34 -0400

    feat: Adding Gift Message column to WooCommerce admin order list view

--- includes/class-gift-message-handler.php
+++ includes/class-gift-message-handler.php
@@ -10,6 +10,13 @@ class WC_Gift_Message_Handler {
         add_filter('woocommerce_add_cart_item_data', [$this, 'save_gift_message_to_cart'], 10, 2);
         add_action('woocommerce_checkout_create_order_line_item', [$this, 'save_gift_message_order'], 10, 4);
 
+        add_filter('woocommerce_get_item_data', [$this, 'display_gift_message_cart'], 10, 2);
+        add_filter('woocommerce_order_item_name', [$this, 'show_gift_message_order'], 10, 2);
+        add_filter('woocommerce_email_order_meta_fields', [$this, 'email_gift_message'], 10, 3);
+
+        add_filter('manage_shop_order_posts_columns', [$this, 'add_admin_column']);
+        add_action('manage_shop_order_posts_custom_column', [$this, 'render_admin_column'], 10, 2);
+
     }
 
     public function add_gift_message_field() {
@@ -51,6 +58,58 @@ class WC_Gift_Message_Handler {
     public function save_gift_message_order($item, $cart_item_key, $values, $order) {
         if (!empty($values['gift_message'])) {
             $item->add_meta_data('Gift Message', $values['gift_message'], true);
+        }
+    }
+
+    public function display_gift_message_cart($item_data, $cart_item) {
+        if (!empty($cart_item['gift_message'])) {
+            $item_data[] = [
+                'name' => 'Gift Message',
+                'value' => wc_clean($cart_item['gift_message']),
+            ];
+        }
+        return $item_data;
+    }
+
+    public function email_gift_message($fields, $sent_to_admin, $order) {
+        foreach ($order->get_items() as $item_id => $item) {
+            $message = wc_get_order_item_meta($item_id, 'Gift Message', true);
+                if ($message) {
+                    $fields[] = [
+                    'label' => 'Gift Message',
+                    'value' => $message,
+                ];
+            }
+        }
+        return $fields;
+    }
+
+    public function show_gift_message_order($item_name, $item) {
+        $message = $item->get_meta('Gift Message');
+            if ($message) {
+            $item_name .= '<br><small><strong>Gift Message:</strong> ' . esc_html($message) . '</small>';
+        }
+        return $item_name;
+    }       
+
+    public function add_admin_column($columns) {
+        $columns['gift_message'] = 'Gift Message';
+        return $columns;
+    }
+
+    public function render_admin_column($column, $post_id) {
+        if ('gift_message' === $column) {
+            $order = wc_get_order($post_id);
+            $messages = [];
+
+            foreach ($order->get_items() as $item) {
+                $message = $item->get_meta('Gift Message');
+                if ($message) {
+                    $messages[] = $message;
+                }
+            }
+
+            echo esc_html(implode(', ', $messages));
         }
     }
 
